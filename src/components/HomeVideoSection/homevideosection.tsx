8f84af05ce3c192f143b36ad34aaa8691269df78
ShuxDEV <70896657+PonchHKT@users.noreply.github.com>
2025-06-10 18:33:34 +0200

    up

--- src/components/HomeVideoSection/homevideosection.tsx
+++ src/components/HomeVideoSection/homevideosection.tsx
@@ -73,10 +73,22 @@ const HomeVideoSection: React.FC = () => {
           Du bon, du bio, de la joie, <br />
           c'est Rosie Trattoria !
         </h1>
-        <p className="description">
-          Savourez l'authenticité italienne avec des produits frais et de
-          saison.
-        </p>
+        <div className="address-container">
+          <svg
+            className="location-icon"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"
+              fill="currentColor"
+            />
+          </svg>
+          <span className="address-text">
+            11 Prom. des Tilleuls, 19100 Brive-la-Gaillarde
+          </span>
+        </div>
         <div className="buttons">
           <a
             href="https://bookings.zenchef.com/results?rid=356394&fullscreen=1"
