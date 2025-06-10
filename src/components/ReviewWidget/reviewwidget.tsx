c70ca39c8db4f8a18efa7a67f5d0dcc005dbfadf
ShuxDEV <70896657+PonchHKT@users.noreply.github.com>
2025-06-11 00:15:29 +0200

    update

--- src/components/ReviewWidget/reviewwidget.tsx
+++ src/components/ReviewWidget/reviewwidget.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
-import "./reviewWidget.scss";
+import "./reviewwidget.scss";
 
 interface Review {
   reviewer: string;
