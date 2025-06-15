db393ab3dddd375c3e665d819dee45e15c9a2cbd
ShuxDEV <70896657+PonchHKT@users.noreply.github.com>
2025-06-15 04:26:10 +0200

    update

--- src/pages/Carte.tsx
+++ src/pages/Carte.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import MenuDisplay from "../components/Carte/MenuDisplay/menudisplay";
+import Swipergallery from "../components/Acceuil/SwiperGallery/swipergallery";
 
 const Carte: React.FC = () => {
   return (
     <>
       <MenuDisplay />
+      <Swipergallery />
     </>
   );
 };
